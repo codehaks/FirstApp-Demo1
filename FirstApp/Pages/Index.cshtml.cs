6638d74c68b21e789c798f1d8003878b8fba1f3c
Hakim Ghods <ghods.hakim@hotmail.com>
2022-01-26 12:35:55 +0330

    Add project files.

---
+++ FirstApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+     
+        public void OnGet()
+        {
+            Length = 9;
+            Width = 5;
+        }
+
+        [BindProperty]
+        public int Length { get; set; }
+
+        [BindProperty]
+        public int Width { get; set; }
+
+        public IActionResult OnPost()
+        {
+            var area = Length * Width;
+            TempData["area"] = area;
+            return RedirectToPage("./result");
+        }
+    }
+}
